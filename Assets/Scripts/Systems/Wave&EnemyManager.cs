fbe4b426e9041e858fe78725a9b097fcca2eb483
SubarashiZeo <70053875+SubarashiZeo@users.noreply.github.com>
2026-05-09 14:52:50 +0800

    minor fix (arrayas almost broke but i found ze problem)+

--- Assets/Scripts/Systems/Wave&EnemyManager.cs
+++ Assets/Scripts/Systems/Wave&EnemyManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:686cce8bc1c965d91abfe7b654efe1ea6abd559853f7e7d9513a30355a7aa1a6
-size 1287
+oid sha256:71dd55048a556e70ac4c60f773e315d23b713de977482c902953be2cfd9948a0
+size 1326
