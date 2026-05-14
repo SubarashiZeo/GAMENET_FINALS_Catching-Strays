52a9abc85fd89d4520ba4e4a1164e46b8beaf9f1
Amirabilis <lianjimulle@gmail.com>
2026-05-14 19:20:04 +0800

    Shotgun Tower Bullet behavior and
    -Bullets now break on contact with the enemy Prefabs
    -Enemies now have health and die when their health goes zero.

--- Assets/Scripts/AI/EnemyType-1.cs
+++ Assets/Scripts/AI/EnemyType-1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b977f7015fa81a4833acffb55d51252b19f9fe0758ae703a6849365c5df26f1
-size 1181
+oid sha256:d0d232d8ba3b8dc5a4d346d0e3eb12d001b6a9953d8cba2db717fd688d39c1c2
+size 2832
--- Assets/Scripts/Tower/ShotgunTower.cs
+++ Assets/Scripts/Tower/ShotgunTower.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ca1073014264a77c052f01acae224040b5f58bcc541210554580930dc836023
-size 3653
+oid sha256:97a1e5f9eba4827328b2a256258aa57c1d0815bcad3f59798ceffce55ace8377
+size 3631
