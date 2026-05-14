659515f42e2c9946b1cce041d635316752641f29
Amirabilis <lianjimulle@gmail.com>
2026-05-14 22:03:45 +0800

    Changed Sniper Tower
    - Deleted the SniperBullet projectile prefab
    - Deleted the SniperBullet cs
    - Change the sniper tower to have a laser that follows the enemy using line renderer.
    - NOTE: there are times when the line doesnt show but the damage still affects the enemies.

--- Assets/Scripts/Tower/SniperBullet.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:793ac7eb005352d00abb2e1c089867825dd5fea7814c4535403bcb14fdc9dba2
-size 1906
--- Assets/Scripts/Tower/SniperTower.cs
+++ Assets/Scripts/Tower/SniperTower.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c682ddf4a06f6348fb991155f689653bae28a92ded0440403818c29d29de1332
-size 3722
+oid sha256:f1d6889e2035ea37425ef7835ba65b207d2ff0ebd0a826bd62fe17eec46f3e12
+size 4520
