7c583931daaf330f5967bea0a6adc5533fcb57b2
Amirabilis <lianjimulle@gmail.com>
2026-05-14 22:21:45 +0800

    SniperTower Fix
    - Fixed the issue where the SniperTower Laser wont render because of the enemy dying and getting destroyed on the killshot.
    - Laser will now linger and still track even if the enemy is destroyed or killed.

--- Assets/Scripts/Tower/SniperTower.cs
+++ Assets/Scripts/Tower/SniperTower.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1d6889e2035ea37425ef7835ba65b207d2ff0ebd0a826bd62fe17eec46f3e12
-size 4520
+oid sha256:7dfb3c0b703a42cc384189a4eba7eefb9625f375457dab0a7e13f413cf0a0d28
+size 4951
