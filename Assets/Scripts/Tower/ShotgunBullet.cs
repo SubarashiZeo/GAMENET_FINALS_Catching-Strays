bb9e9f8fbd22b2d6e167f36844f91232650d5a79
Amirabilis <lianjimulle@gmail.com>
2026-05-14 16:43:26 +0800

    Firing and targeting for Spray Bottle Tower
    - Added firing, targeting and bullets to the Spray Tower (Shotgun)

---
+++ Assets/Scripts/Tower/ShotgunBullet.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:89ee76af685e8f1d2c4c06655611376cf99a6ae76c97f0183770cf550ba3ea82
+size 1560
---
+++ Assets/Scripts/Tower/ShotgunTower.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7ca1073014264a77c052f01acae224040b5f58bcc541210554580930dc836023
+size 3653
