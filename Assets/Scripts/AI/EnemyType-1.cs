533ade0d1e1eafa8a68d6b7ded38828aa118b95c
kryulaa <kryu_maro_veracruz@dlsl.edu.ph>
2026-05-14 19:26:35 +0800

    Level design and stuff

--- Assets/Scripts/AI/EnemyType-1.cs
+++ Assets/Scripts/AI/EnemyType-1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b977f7015fa81a4833acffb55d51252b19f9fe0758ae703a6849365c5df26f1
-size 1181
+oid sha256:19d744bc2215c1907df4fd5be6e8f7213f0be26ce49b4dbb647ac7556560cc49
+size 2576
---
+++ Assets/Scripts/AI/PathRenderer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c09d8a873d31f26f10174d8f2e07e7ca2d8dcc35209fd6de4e599f57c5206501
+size 2923
